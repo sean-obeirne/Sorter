2d19f4f245a704a0f23a27279f8b4dead9b91abb
Sean O'Beirne <sean.t.obeirne@gmail.com>
2021-05-19 15:24:05 -0400

    implemented heap sort

--- sorter.py
+++ sorter.py
@@ -1,5 +1,6 @@
 import pygame, sys, os, random, time, threading
 
+# Color constants
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 DGRAY = (20,20,20)
@@ -9,25 +10,34 @@ GREEN = (0,255,0)
 RED = (255,0,0)
 DRED = (100,20,20)
 
+# Window property / layout constants
 RES_WID = 1100
 RES_HGT = 800
 RESOLUTION = [RES_WID,RES_HGT]
 TOP_BAR_HGT = 100
 
+# Initialize game
 pygame.init()
 screen = pygame.display.set_mode(RESOLUTION)
 
+# Initialize font
 pygame.font.init()
 font = pygame.font.SysFont('monospace', 40)
-# font.set_bold(True)
+
+
+####################
+# Global Variables #
+####################
 
 running = True
 sorting = False
+
 arr_ct = 2
 arr = list()
-UI_x_offset = 0
-bar_x_offset = 38
-UI_x_cur = 25 + UI_x_offset
+
+UI_x_offset = 0     # how far from left of screen UI begins
+bar_x_offset = 38   # how far from left of screen bars are drawn
+UI_x_cur = 25 + UI_x_offset # next UI component's x value
 UI_components = list()
 
 
@@ -59,6 +69,7 @@ class Component:
         text_rect = self.textsurface.get_rect(center=(self.wid/2 + self.x, self.hgt/2 + self.y))
         surface.blit(self.textsurface,text_rect)
 
+# A Bar is simple a white bar to be drawn in order to reflect some value in our list to be sorted
 class Bar:
     def __init__(self, val, factor=20, bcolor=WHITE, tcolor=BLACK):
         self.val = val
@@ -72,12 +83,12 @@ class Bar:
         text_rect = self.textsurface.get_rect(center=(wid/2 + x, self.hgt/2 + y))
         # surface.blit(self.textsurface,text_rect)
 
-
+# Randomize the order of bars so they may be sorted
 def randomize_bars():
     global arr
     arr = sorted(arr, key = lambda x: random.random() )
 
-
+# Initialize array of Bars
 def init_bars():
     arr.clear()
     fact = 600 / arr_ct
@@ -85,19 +96,22 @@ def init_bars():
         arr.append(Bar(i, factor=fact))
     randomize_bars()
 
+# Intelligently draw bars within the confines of the screen
 def draw_bars():
     global arr
-    bwidth = int((1024 / arr_ct) - 2)
+    bwidth = int((1024 / arr_ct) - 1)
     x_cur = bar_x_offset
     for bar in arr:
         bar.draw(screen, x_cur, TOP_BAR_HGT, bwidth)
-        x_cur += 2 + bwidth
+        x_cur += 1 + bwidth
 
 
 ####################
 # Button Functions #
 ####################
 
+# Divide the number of total Bars by 2
+#   comp: the component to change the text of
 def div2(comp):
     global arr_ct
     if not sorting and arr_ct > 2:
@@ -105,19 +119,23 @@ def div2(comp):
         comp.set_text(str(arr_ct))
         init_bars()
 
+# Multiply the number of total Bars by 2
+#   comp: the component to change the text of
 def mul2(comp):
     global arr_ct
-    if not sorting and arr_ct < 256:
+    if not sorting and arr_ct < 512:
         arr_ct *= 2
         comp.set_text(str(arr_ct))
         init_bars()
 
+# Cancel all sorting and reinitialize the Bars on screen
 def reset():
     global sorting
     sorting = False
     init_bars()
     pass
 
+# Quick Sort #
 # recursive quicksort helper
 def qsort():
     global arr
@@ -146,18 +164,67 @@ def quick_sort():
 
     sorting = False
 
+
+# Heap Sort #
+# Heapify the node at index i in heap of length n
+#   i: the index of the root of this heap
+#   n: the bound for how large the heap is
+def heapify(i, n):
+    global arr
+
+    largest = i
+    li = 2*i + 1
+    ri = 2*i + 2
+
+    # check if left exists and is bigger than root
+    if li < n and arr[li].val > arr[largest].val:
+        largest = li
+
+    # check if right exists and is bigger than root
+    if ri < n and arr[ri].val > arr[largest].val:
+        largest = ri
+
+    # swap if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        time.sleep(1/(arr_ct))
+        heapify(largest, n)
+
+# Build the max heap using heapify, then extract the root and heapify.
+# Repeat to extract all values in order
 def heap_sort():
     global sorting
     global arr
     sorting = True
-    pass
 
+    n = arr_ct
+
+    # Build max heap
+    for i in range(n//2 - 1, -1, -1):
+        heapify(i, n)
+        if not sorting:
+            return
+
+    # Extract top
+    while n > 0:
+        arr[n-1], arr[0] = arr[0], arr[n-1]
+        heapify(0, n-1)
+        n -= 1
+        if not sorting:
+            return
+
+    sorting = False
+
+# Merge Sort #
 def merge_sort():
     global sorting
     global arr
     sorting = True
-    pass
 
+    sorting = False
+
+# Bubble Sort #
+# Loop through the list, swapping each entry which is larger than its successor
 def bubble_sort():
     global sorting
     global arr
@@ -165,12 +232,10 @@ def bubble_sort():
 
     for bar in arr:
         for i in range(len(arr)-1):
-            if sorting == False:
+            if not sorting:
                 return
             if arr[i].val > arr[i+1].val:
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
+                arr[i], arr[i+1] = arr[i+1], arr[i]
                 time.sleep(1/(arr_ct*2))
 
     sorting = False
@@ -233,16 +298,19 @@ while(running):
                     mul2(arr_ct_comp)
                 elif reset_comp.Component.collidepoint(mpos):
                     reset()
-                elif quick_comp.Component.collidepoint(mpos):
-                    quick_sort()
-                elif heap_comp.Component.collidepoint(mpos):
-                    heap_sort()
-                elif merge_comp.Component.collidepoint(mpos):
-                    merge_sort()
-                elif bubble_comp.Component.collidepoint(mpos):
-                    th = threading.Thread(target=bubble_sort)
-                    th.start()
-                    # bubble_sort()
+                if not sorting:
+                    if quick_comp.Component.collidepoint(mpos):
+                        th = threading.Thread(target=quick_sort)
+                        th.start()
+                    elif heap_comp.Component.collidepoint(mpos):
+                        th = threading.Thread(target=heap_sort)
+                        th.start()
+                    elif merge_comp.Component.collidepoint(mpos):
+                        th = threading.Thread(target=merge_sort)
+                        th.start()
+                    elif bubble_comp.Component.collidepoint(mpos):
+                        th = threading.Thread(target=bubble_sort)
+                        th.start()
                 break
 
 
